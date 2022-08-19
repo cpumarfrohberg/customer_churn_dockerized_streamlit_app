ab9ae81669415f7d0cf66c9f3ee9af2da395473d
cpumarfrohberg <cpumarfrohberg@gmail.com>
2022-08-19 15:28:07 +0200

    updated README, renamed app script

--- README.md
+++ README.md
@@ -4,46 +4,30 @@ This project is based on a cooperation with a Savings Bank ("Caja Municipal de A
 
 <img src ="images/CMAC.jpg" width = "100">
 
-* 3 models have been compared to each other:
-* 1. a Logistic Regression classifier
-* 2. a Decision Tree classifier and
-* 3. a Random Forest classifier.
-
-* The features used can be grouped into three categories:
+* Initial features used can be grouped into three categories:
 * 1. socioeconomic features
 * 2. variables related to the Loan Officers (LO) attending SME clients and 
 * 3. variables related to the so-called "RFM"- methodology often used in practice for classifying customers into different groups. 
 
-* Model	performance (AUC)
-**	RF	0.94
-**	Tree	0.93
-**	LogReg	0.88 
-
-* Model	performance (f1 - score)
-** RF	0.94
-** Tree	0.92
-** LogReg	0.85
-
-* Based on an analysis of feature importance, only 5 features are relevant for this classification problem, which can be grouped into time-related features ("year" and "month"), as well as in features related to the relationship between customers and their client advisors. 
-* A particular challenge was posed by the strong amount of duplicates, which has lead to rather balanced classes; this is untypical for a churn problem, and indicates that the data potentially needs to be tidied more in future.
+* However, and based on model fit of a Random Forest Classifier, the following features resulted as having greatest importance
+* 1. what was the employment status of the LO 6 months prior?
+* 2. what is the expectation of the app user regarding the employment status of the LO 3 months into the future?
+* 3. what is the expectation of the app user regarding the employment status of the LO 6 months into the future?
 
 ## Content of the project
 * 1. data directory
 * 2. EDA (in separate notebook)
-* 3. trained models
+* 3. model fit (including feature importance with Random Forest Classifier)
 * 4. artefacts folder containing pickled model
+* 5. streamlit app
 
 ## In order to run the models
 - clone repo locally
 - create an environment with the contents of the requirements.txt file (if you are using conda: install pip first via "conda install pip" and then "pip install -r requirements.txt")
 
 ## Future Updates
-- [ ] deploy locally using a flask app
-- [ ] dockerize and deploy on AWS Elastic Beanstalk 
-- [ ] run models with a dataset representative of a churn problem (imbalanced dataset)
+- [ ] dockerize
 - [ ] run Artificial Neural Network
-- [ ] expand analysis by including time series of clients' transactions with Savings Bank in order to predict potential loan amounts to be requested by recurrent customers of the institution
-
 
 ## Author
 
--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-import streamlit as st
+from ast import For
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
+import streamlit as st
 
-from sklearn.linear_model import LogisticRegression
+import pickle
+
+MODEL_FILE = "./artefacts/churn-model.bin"
+INDEX = [0]
 
 
-features = pd.read_csv("./data/Tabla_01_English_Unique_postEDA.csv", sep = ",", encoding="latin1" , index_col=0, parse_dates=True)
-churn_history = pd.read_csv("./data/Tabla_02_Clients_English.csv", sep = ",", encoding="latin1" , index_col=0, parse_dates=True)
-rfm = df = pd.read_csv("./data/Tabla_01_English_20211020.csv", sep =",", encoding= "latin1", index_col=0, parse_dates=True) 
+features = pd.read_csv("./data/Tabla_01_English_Unique_postEDA.csv", encoding="latin1" , index_col=0, parse_dates=True)
+churn_history = pd.read_csv("./data/Tabla_02_Clients_English.csv", encoding="latin1" , index_col=0, parse_dates=True)
+rfm = df = pd.read_csv("./data/Tabla_01_English_20211020.csv", encoding= "latin1", index_col=0, parse_dates=True) 
 
 rfm["Year"] = rfm.index.year
 rfm["Month"] = rfm.index.month
-
-
-# clf = LogisticRegression()
 
 st.title("Customer Churn Predictor")
 
@@ -27,7 +27,7 @@ if nav == "Home":
     st.markdown(
     """ ## Welcome to the Customer Churn Predictor page.
     ##### This project was implemented jointly with a Savings Bank in Peru.
-    ##### Its scope consisted in identifying the most relevant features in predicting churn of SME clients as well as making predictions three months into the future.
+    ##### Its main objective consists in identifying the most relevant features in predicting churn of SME clients as well as making predictions three months into the future.
     """
     )
     st.image("CMAC.jpg", width=100)
@@ -35,20 +35,19 @@ if nav == "Home":
 if nav == "EDA":
     st.write("Welcome to the section on Exploratory Data Analysis.")
     if st.checkbox("Click here to see the time series of customer churn"):
-        val = st.slider("Filter data using years", 2018, 2021)
-        p = churn_history["Client_Churn"]
-        churn_data = p.loc[churn_history.index.year >= val]
-        st.line_chart(churn_data)
-    if st.checkbox("Click here to see the time series of monetary features"):
-        val = st.slider("Filter data using years", 2018, 2021)
-        p2 = rfm.groupby(["Year","Month"])["n(Loans)_Outstanding_Maynas"].mean()
-        churn_data = p2.loc[churn_history.index.year >= val]
-        st.line_chart(churn_data)
-    # if st.checkbox("Click here to see the distribution of socioeconomic features"):
+        val = st.slider(
+            "Filter data using years", 
+            min_value = 2018, 
+            max_value = 2021
+            )
+        churn_series = churn_history["Client_Churn"]
+        churn_series = churn_series[churn_history.index.year >= val]
+        st.line_chart(churn_series)
+    # if st.checkbox("Click here to see the time series of monetary features"):
     #     val = st.slider("Filter data using years", 2018, 2021)
-    #     p3 = churn_history["Client_Churn"]
-    #     churn_data = p3.loc[churn_history.index.year >= val]
-    #     st.line_chart(churn_data)
+    #     monetary_series = rfm.groupby(["Year"])["n(Loans)_Outstanding_Maynas"].mean()
+    #     monetary_series = monetary_series[monetary_series.index >= val]
+    #     st.line_chart(monetary_series)
 
 
 if nav == "Prediction":
@@ -56,16 +55,62 @@ if nav == "Prediction":
     """ #### Welcome to the predictions page.
     """
     )
-    if st.checkbox("If you are interested to see features used for training the model, click here:"):
+    
+    if st.checkbox("<- If you are interested to see the initial data made available by the Savings Bank, click here"):
         st.table(features)
-    val = st.number_input("Enter the amount of months that the Loan Officer has left your institution",
-    0,
-    60,
-    step = 1)
-    # pred = clf.predict(X_test)[0]
+    
+    st.markdown(
+    """ ##### For predictions regarding your client's status in 3 months' time, please answer the following three questions. Select '0' for 'no' and '1' for 'yes'.
+    """
+    )
 
-    # if st.button("Predict"):
-    #     st.success(f“The predicted probability of churn is {round(pred)}:)
+    # TODO: include table, but based on the main features, i.e. the features used for model fit
+    @st.cache
+    def load_model():
+        print("loading the model")
+        with open(MODEL_FILE, "rb") as file_in:
+            clf_LR = pickle.load(file_in)
+        return clf_LR
+    
+    clf_LR = load_model()
+
+    col1, col2, col3 = st.columns(3)
+
+    active_past_6 = col1.number_input(
+        "Was the Loan Officer employed 6 months ago?", 
+        min_value = 0,
+        max_value = 1,
+        )
+    active_post_3 = col2.number_input(
+        "Do you expect the Loan Officer to still be employed in 3 months time?", 
+        min_value = 0,
+        max_value = 1,
+        )
+    active_post_6 = col3.number_input(
+        "Do you expect the Loan Officer to still be employed in 6 months time?", 
+        min_value = 0,
+        max_value = 1,
+        )
+
+    user_input = {
+        "LO_active_past_6": int(active_past_6),
+        "LO_active_post_3": int(active_post_3),
+        "LO_active_post_6": int(active_post_6)
+        }
+
+    user_input = pd.DataFrame(
+        user_input, 
+        columns = user_input.keys(),
+        index = INDEX
+        )
+
+    user_input = np.array(user_input).reshape(1, -1)
+
+    pred = clf_LR.predict(user_input)[0]
+    proba = clf_LR.predict_proba(user_input)[0]
+
+    if st.button("Predict"):
+        st.success(f'Your client will leave your institution in three months time with a probability of: {round(proba[1], 2)}')
 
 if nav == "Contribute":
     st.header("Thank you for contributing to our dataset.")
