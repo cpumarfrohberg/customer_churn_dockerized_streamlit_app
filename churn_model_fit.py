20556f0748472e94a856f787167e494aa87f326d
Carlos Pumar <carlospumar@Carloss-MacBook-Pro.local>
2022-08-22 17:26:28 +0200

    adding changes for refactoring

--- churn_model_fit.py
+++ churn_model_fit.py
@@ -19,10 +19,10 @@ clf_LR = LogisticRegression(class_weight = WEIGHTS, random_state=42)
 
 time.sleep(2)
 print("reading in dataset")
-df = pd.read_csv("../data/Tabla_01_English_Unique_postEDA.csv", index_col=0, parse_dates=True)
+df = pd.read_csv("./data/Tabla_01_English_Unique_postEDA.csv", index_col=0, parse_dates=True)
 
-print("splitting data")
 time.sleep(2)
+print("splitting data")
 X = df[["LO_Active_Employee_Post3Months", "LO_Active_Employee_Prior6Months","LO_Active_Employee_Post6Months"]] # features selected based on calculation of feature importance in NB "all features".
 y = df["Client_Status_Post3Months"]  
 
@@ -45,16 +45,13 @@ fit_model_X_val = cmf.model_fit(
 
 time.sleep(2)
 print("making predictions on X_train and X_val")
-pred_X_train = predictions(
+pred_X_train = cmf.predictions(
     fit_model = fit_model_X_train,
-    X = X_train,
-    y = y_train
+    X = X_train
     )
-pred_X_val = predictions(
+pred_X_val = cmf.predictions(
     fit_model = fit_model_X_val,
-    X = X_val,
-
-    y = y_val
+    X = X_val
     )
 f1_score_train = f1_score(y_train, pred_X_train).round(2)
 f1_score_val = f1_score(y_val, pred_X_val).round(2)
@@ -77,7 +74,7 @@ print(f"Area Under Curve (validation set): {roc_auc_score(y_val, pred_X_val).rou
 
 time.sleep(2)   
 print("proceeding to refit model on complete dataset")
-fit_model_X = model_fit(
+fit_model_X = cmf.model_fit(
     model = clf_LR,
     X = X, 
     y = y
@@ -85,10 +82,10 @@ fit_model_X = model_fit(
 
 time.sleep(2)   
 print("saving full model")
-with open("../artefacts/churn-model.bin", "wb") as f_out:
+with open("./artefacts/churn-model-refactored.bin", "wb") as f_out:
     pickle.dump(fit_model_X, f_out) 
 time.sleep(2)   
-print("full model saved as churn-model.bin")
+print("full model saved as churn-model-refactored.bin")
 
 
 
